0a3498b2b1ad400ecb1673ab9356d5337373f0f9
woodjeryvolcy <woodjery.volcy@edu.ufes.br>
2026-03-29 15:37:01 -0300

    rev2 resolvido

--- rev2.c
+++ rev2.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 
 int main(){
+    // Variáveis para o contador e o número de linhas
     int contador = 1;
-    int numero =5; 
+    int numero ; 
+    //Solicitar ao usuário o número de linhas para a pirâmide
+    scanf("%d", &numero);
+    //Gerar a pirâmide de números
     for (int i = 1; i <= numero; i++) {
         for (int j = 1; j <= i; j++) {
             printf("%d ", contador);
@@ -11,6 +15,6 @@ int main(){
         }
         printf("\n");
     }
-
+    //Finalizar o programa
     return 0;
 }
