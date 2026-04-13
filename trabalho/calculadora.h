39786d5fd165d16ce9e9bd4251d9a0b8b971cee7
woodjeryvolcy <woodjery.volcy@edu.ufes.br>
2026-04-13 09:16:11 -0300

    Removendo pasta trabalho

--- trabalho/calculadora.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CALCULADORA_H
-#define _CALCULADORA_H
-
-float soma(float a, float b);
-float subtrai(float a, float b);
-float multiplica(float a, float b);
-float divide(float a, float b);
-
-#endif
